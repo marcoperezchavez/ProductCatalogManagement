f6aa1d659589cf8862cf79bde18e831d3909439c
Marco Antonio de Jesús Pérez Chávez C <mperezchavez@coca-cola.com>
2025-03-27 00:39:58 -0600

    UPDATE ProductContext ADDED migrations decimal problem FIXED

--- ProductCatalogManagement.Server/Data/ProductContext.cs
+++ ProductCatalogManagement.Server/Data/ProductContext.cs
@@ -8,5 +8,12 @@ namespace ProductCatalogManagement.Server.Data
         public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+        }
     }
 }
